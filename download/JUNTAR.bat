2f1319285c52958e8538484cab822d7114f12574
AlexCromy <alexcromy@gmail.com>
2026-09-07 20:31:19 -0300

    juntador

---
+++ download/JUNTAR.bat
@@ -0,0 +1,6 @@
+@echo off
+title Nexus Arcade - montar programa
+copy /b NexusArcade.zip.part-aa + NexusArcade.zip.part-ab + NexusArcade.zip.part-ac + NexusArcade.zip.part-ad + NexusArcade.zip.part-ae + NexusArcade.zip.part-af + NexusArcade.zip.part-ag + NexusArcade.zip.part-ah + NexusArcade.zip.part-ai + NexusArcade.zip.part-aj Nexus-Arcade-Windows-x64.zip
+echo.
+echo Pronto! Extraia Nexus-Arcade-Windows-x64.zip e abra "Nexus Arcade.exe".
+pause
